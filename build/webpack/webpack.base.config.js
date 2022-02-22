1b24f3349c99071a8f121e86da90cf139969a91a
孟刘 <171626037@qq.com>
2022-02-22 22:56:10 +0800

    配置脚手架

---
+++ build/webpack/webpack.base.config.js
@@ -0,0 +1,130 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FriendlyErrorWebpackPlugin = require("friendly-errors-webpack-plugin");
+const webpackBar = require("webpackbar");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: {
+    path: path.resolve(__dirname, "../../src/index"),
+  },
+  output: {
+    path: path.resolve(__dirname, "../../dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx", "css", "less"],
+    alias: {
+      "@src": `${process.cwd()}/src`,
+    },
+  },
+  // node: {
+  //     global: false,
+  //     __filename: true,
+  //     __dirname: true
+  // },
+  //   resolve: {
+  //     alias: {
+  //       "@src": path.resolve(__dirname, "../src"),
+  //       "@pages": path.resolve(__dirname, "../src/pages"),
+  //       "@api": path.resolve(__dirname, "../src/api"),
+  //       "@utils": path.resolve(__dirname, "../utils"),
+  //     },
+  //     extensions: [".js", ".jsx", ".ts", ".tsx", "css", "less"],
+  //   },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/, // .ts或者tsx后缀的文件，就是typescript文件
+        use: "ts-loader", // 就是上面安装的ts-loader
+        exclude: "/node-modules/", // 排除node-modules目录
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "../",
+            },
+          },
+          {
+            loader: "css-loader",
+            options: {
+              //   esModule: false, //解决背景图乱码，生成多余文件问题
+            },
+          },
+        ],
+      },
+      {
+        test: /\.less$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "../",
+            },
+          },
+          {
+            loader: "css-loader",
+            options: {
+              //   esModule: false, //解决背景图乱码，生成多余文件问题
+            },
+          },
+          {
+            loader: "less-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              //   limit: 2000,
+              name: "img/[name].[hash:8].[ext]",
+              esModule: false,
+            },
+          },
+        ],
+        type: "javascript/auto",
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      //       {
+      //         test: /\.less$/,
+      //         use: ["style-loader", "css-loader", "less-loader"],
+      //       },
+      //       {
+      //         test: /\.(js|jsx|ts|tsx)$/,
+      //         use: ["babel-loader"],
+      //         exclude: /node_modules/,
+      //       },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "test html-webpack-plugin",
+      template: path.resolve(__dirname, "../../public/index.html"),
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/index.css",
+    }),
+    // 错误提示插件，用于在控制台展示比较友好的错误提示
+    new FriendlyErrorWebpackPlugin({
+      clearConsole: true,
+    }),
+    new webpackBar({
+      name: "Client 代码编译",
+      color: "#d3adf7",
+    }),
+    //   new webpack.DefinePlugin({
+    //     "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+    //   }),
+  ],
+};
---
+++ build/webpack/webpack.dev.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+const BaseConfig = require("./webpack.base.config");
+const config = require("../../config");
+
+module.exports = merge(BaseConfig, {
+  mode: "development",
+  devServer: {
+    // static: false,
+    static: {
+      // static: ['assets']
+      // directory: path.join(__dirname, "../../src"),
+    },
+    hot: true,
+    open: true,
+    host: config.dev.host,
+    port: config.dev.port,
+    // proxy: config.proxy,
+    // historyApiFallback: true,
+    // contentBase: `${process.cwd()}/public`,
+  },
+});
